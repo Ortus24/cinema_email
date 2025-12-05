efeda3cfb2d81a3dad360fbf88bb41427b9afe37
Ortus24 <nhatlckbt007@gmail.com>
2025-12-05 14:50:31 +0700

    limit ignoreTLS

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,6 +12,7 @@ export class AppService {
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    ignoreTLS: false,
     auth: {
       user: 'nhatnlhe186939@fpt.edu.vn',
       pass: 'tahjapoghwvglfzp',
